fbe7f980198ff5867a01ef85e8b4b5abf96391da
neel1996 <47709856+neel1996@users.noreply.github.com>
2023-10-11 00:39:00 +0530

    Update documentation

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# React + Vite
+# Lagoon
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A simple search tool for github documents powered by supabase and llama2
 
-Currently, two official plugins are available:
+## Getting Started
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### React App
+
+- Create a supabase account and setup a new project
+- Use the [supabase.sql](supabase.sql) file to create the database schema
+- Create a `.env` file in the root of the project and add the following variables:
+
+```
+VITE_SUPABASE_URL=<supabase url>
+VITE_SUPABASE_KEY=<supabase key>
+```
+
+- Run `yarn` to install dependencies
+- Run `yarn dev` to start the dev server
+
+### Python inference api
+
+- `cd inference-server`
+- Create a virtual environment `python3 -m venv venv`
+- Activate the virtual environment `source venv/bin/activate`
+- Install dependencies `pip install -r requirements.txt`
+- Run the server `python inference.py`
+
+> The first startup will take a few minutes to download the model and load it into memory
+
+## Demo
+
+![demo](demo.gif)
--- supabase.sql
+++ supabase.sql
@@ -1,3 +1,55 @@
+create table
+  public.repositories (
+    id uuid not null default gen_random_uuid (),
+    name text not null,
+    org text not null,
+    is_disabled boolean null default false,
+    created_at timestamp with time zone not null default now(),
+    created_by uuid not null default auth.uid (),
+    constraint repositories_pkey primary key (id),
+    constraint repositories_name_key unique (name)
+  ) tablespace pg_default;
+
+
+create table
+  public.documents (
+    id uuid not null default gen_random_uuid (),
+    repo_id uuid not null,
+    document_name text not null,
+    document_content text not null,
+    created_at timestamp with time zone not null default now(),
+    created_by uuid not null default auth.uid (),
+    updated_at timestamp with time zone null,
+    constraint documents_pkey primary key (id),
+    constraint documents_repo_id_fkey foreign key (repo_id) references repositories (id) on delete cascade
+  ) tablespace pg_default;
+
+create trigger "embed-documents"
+after insert
+or
+update on documents for each row
+execute function supabase_functions.http_request (
+  'https://<project_ref>.functions.supabase.co/embed-documents',
+  'POST',
+  '{"Content-type":"application/json"}',
+  '{}',
+  '5000'
+);
+
+
+create table
+  public.document_embeddings (
+    document_id uuid not null,
+    document_name text not null,
+    embeddings public.vector not null,
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone null,
+    document_content text null,
+    constraint document_embeddings_pkey primary key (document_id),
+    constraint document_embeddings_document_id_fkey foreign key (document_id) references documents (id) on delete cascade
+  ) tablespace pg_default;
+
+
 create or replace function match_documents (
   query_embedding vector(384),
   match_threshold float,
